c411c9b518f28e765387b069c889dda61e506bae
tjhootman <tjhootman@gmail.com>
2025-05-10 09:39:29 -0400

    Add RequestException in case of failure during API call

--- java_repos_visuals.py
+++ java_repos_visuals.py
@@ -4,10 +4,15 @@ import plotly.express as px
 # Make an API call and check the response.
 url = "https://api.github.com/search/repositories"
 url += "?q=language:java+sort:stars+stars:>10000"
-
 headers = {"Accept": "application/vnd.github.v3+json"}
-r = requests.get(url, headers=headers)
-print(f"Status code: {r.status_code}")
+
+try:
+    r = requests.get(url, headers=headers)
+    r.raise_for_status() # raise an exception for bad status codes
+    print(f"Status code: {r.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Error during API request: {e}")
+    exit()
 
 # Process overall results
 response_dict = r.json()
